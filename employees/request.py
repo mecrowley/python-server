bb83ccb605c9a9395b120365d3e8a721cb78f983
Madeline Crowley <madelinecrowley@gmail.com>
2021-07-12 15:34:55 -0500

    first commit

---
+++ employees/request.py
@@ -0,0 +1,13 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Brenda",
+        "locationId": 1
+    }
+]
+
+
+def get_all_employees():
+    """Returns a list of the employees
+    """
+    return EMPLOYEES
---
+++ locations/request.py
@@ -0,0 +1,13 @@
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville North",
+        "address": "300 North Parkway"
+    }
+]
+
+
+def get_all_locations():
+    """Returns a list of the locations
+    """
+    return LOCATIONS

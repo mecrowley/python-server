ba02eacdba9d290d4594a3ba2698a8ab745e9b0d
Madeline Crowley <madelinecrowley@gmail.com>
2021-07-13 10:35:35 -0500

    CH 3 added get functionality for animals/employees/locations

--- employees/request.py
+++ employees/request.py
@@ -11,3 +11,18 @@ def get_all_employees():
     """Returns a list of the employees
     """
     return EMPLOYEES
+
+def get_single_employee(id):
+    # Variable to hold the found employee, if it exists
+    requested_employee = None
+
+    # Iterate the EMPLOYEES list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for employee in EMPLOYEES:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if employee["id"] == id:
+            requested_employee = employee
+
+    return requested_employee
+    
--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals
-from employees import get_all_employees
-from locations import get_all_locations
+from animals import get_all_animals, get_single_animal
+from employees import get_all_employees, get_single_employee
+from locations import get_all_locations, get_single_location
 
 
 # Here's a class. It inherits from another class.
@@ -13,6 +13,27 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It gives a description of the class or function
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
+
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
 
     # Here's a class function
     def _set_headers(self, status):
@@ -45,24 +66,31 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_GET(self):
         """Handles GET requests to the server
         """
-        # Set the response code to 'Ok'
         self._set_headers(200)
+        response = {}
 
-        # Your new console.log() that outputs to the terminal
-        print(self.path)
+        # Parse the URL and capture the tuple that is returned
+        (resource, id) = self.parse_url(self.path)
 
-        # It's an if..else statement
-        if self.path == "/animals":
-            response = get_all_animals()
-        elif self.path == "/employees":
-            response = get_all_employees()
-        elif self.path == "/locations":
-            response = get_all_locations()
-        else:
-            response = []
+        if resource == "animals":
+            if id is not None:
+                response = f"{get_single_animal(id)}"
+            else:
+                response = f"{get_all_animals()}"
+        elif resource == "employees":
+            if id is not None:
+                response = f"{get_single_employee(id)}"
+            else:
+                response = f"{get_all_employees()}"
+        elif resource == "locations":
+            if id is not None:
+                response = f"{get_single_location(id)}"
+            else:
+                response = f"{get_all_locations()}"
+
 
         # This weird code sends a response back to the client
-        self.wfile.write(f"{response}".encode())
+        self.wfile.write(response.encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
